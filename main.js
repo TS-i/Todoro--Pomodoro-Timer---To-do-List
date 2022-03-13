acb2c0c792157ae06b328cb67f07d49a23f06337
BizzleBo <teeninenineyt@gmail.com>
2022-03-12 18:18:54 -0800

    Pomodoro Counter, To-Do list, & some styling added

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
-//import './style.css'
+import './style.css'
 
+//--Timer Code--//
+// Get Timer buttons from HTML
 var startStopButton = document.querySelector('#startStop');
 var resetButton = document.querySelector("#reset");
 
+// Add EventListeners to Timer buttons
 startStopButton.addEventListener('click', startTimer);
 resetButton.addEventListener('click', resetTimer);
 
+// Get Pom counter from HTML
+var pomCounter = document.querySelector("#pomCounter");
+var pomCount = 0;
 
+// Set up Timer Variable for later use
 let intervalID = null;
 
+// Build Timer Function
 var sec = '00';
 var min = 25;
 var timer = function(){
@@ -17,14 +25,16 @@ var timer = function(){
     if (sec < 0) {
         min--;
         sec=59;
-    }
-    if (min<0 & sec<0){
-      clearInterval(timer);
+    } else if (min == 0 && sec == 0){
+      resetTimer();
+      pomCount++;
+      pomCounter.innerHTML="Pomodoro Counter : " + pomCount;
     }
 };
 
+// Start Timer Button
 function startTimer(){
-  intervalID = setInterval(timer, 1000);
+  intervalID = setInterval(timer, 1);
   console.log("Started");
   startStopButton.removeEventListener("click", startTimer);
   startStopButton.addEventListener("click", stopTimer);
@@ -32,6 +42,7 @@ function startTimer(){
   startStopButton.innerHTML = "Stop";
 }
 
+// Stop Timer Button
 function stopTimer(){
   clearInterval(intervalID);
   console.log("Stopped");
@@ -41,9 +52,77 @@ function stopTimer(){
   startStopButton.innerHTML = "Start";
 }
 
+// Reset Timer Button
 function resetTimer(){
-  console.log("Timer Reset");
+  clearInterval(intervalID);
   sec = '00';
   min = 25;
-clock.innerHTML=min+':'+sec;
+  clock.innerHTML=min+':'+sec;
+  console.log("Timer Stopped & Reset");
+  startStopButton.removeEventListener("click", stopTimer);
+  startStopButton.addEventListener("click", startTimer);
+  startStopButton.value = "Start";
+  startStopButton.innerHTML = "Start";
+}
+
+//--To-Do List Code--//
+
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Get Add Button From HTML
+var addButton = document.querySelector("#addBtn");
+addButton.addEventListener('click', newItem);
+
+// Create a new list item when clicking on the "Add" button
+function newItem() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("todo").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
 }
