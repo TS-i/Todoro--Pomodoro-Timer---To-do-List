6d206da0c8eb6dd6ea66b68ae412d01708bb864c
BizzleBo <teeninenineyt@gmail.com>
2022-03-12 16:07:27 -0800

    Start/Stop & Reset Buttons added

--- main.js
+++ main.js
@@ -1,18 +1,49 @@
 //import './style.css'
 
-function timer(){
-  var sec = '00';
-  var min = 25;
-  var timer = setInterval(function(){
-      clock.innerHTML=min+':'+sec;
-      sec--;
-      if (sec < 0) {
-          min--;
-          sec=59;
-      }
-      if (min<0 & sec<0){
-        clearInterval(timer);
-      }
-  }, 100);
+var startStopButton = document.querySelector('#startStop');
+var resetButton = document.querySelector("#reset");
+
+startStopButton.addEventListener('click', startTimer);
+resetButton.addEventListener('click', resetTimer);
+
+
+let intervalID = null;
+
+var sec = '00';
+var min = 25;
+var timer = function(){
+    clock.innerHTML=min+':'+sec;
+    sec--;
+    if (sec < 0) {
+        min--;
+        sec=59;
+    }
+    if (min<0 & sec<0){
+      clearInterval(timer);
+    }
+};
+
+function startTimer(){
+  intervalID = setInterval(timer, 1000);
+  console.log("Started");
+  startStopButton.removeEventListener("click", startTimer);
+  startStopButton.addEventListener("click", stopTimer);
+  startStopButton.value = "Stop";
+  startStopButton.innerHTML = "Stop";
 }
 
+function stopTimer(){
+  clearInterval(intervalID);
+  console.log("Stopped");
+  startStopButton.removeEventListener("click", stopTimer);
+  startStopButton.addEventListener("click", startTimer);
+  startStopButton.value = "Start";
+  startStopButton.innerHTML = "Start";
+}
+
+function resetTimer(){
+  console.log("Timer Reset");
+  sec = '00';
+  min = 25;
+clock.innerHTML=min+':'+sec;
+}
